3923f82e80e4d02ac03183a3a27b8d7b7f124d77
garrik sturges <garriksturges@mac.com>
2011-12-15 21:08:59 -0800

    firefox fix

--- app/assets/javascripts/document.js
+++ app/assets/javascripts/document.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	$("#dropDownTable tr td").hover(
 		function(){$(this).find("li").toggle();}
+		//function(){$(this).find("li").css('display','table-row');},
+		//function(){$(this).find("li").css('display','none');}
 		);
 	$("table.topTabs").tabs("div.topPanes > div");
 	$(".slidetabs").tabs(".pictures > img", {
